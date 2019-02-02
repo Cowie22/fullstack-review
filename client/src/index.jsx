9a1bb04c51d604bf444bee29255b69116ca88a66
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-02-01 20:11:41 -0800

    Set up mongo db and fixed bug in set-state

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,22 +7,23 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
+    this.search = this.search.bind(this);
 
   }
 
-  search (term) {
+  search(term) {
     console.log(`${term} was searched`);
     // TODO
   }
 
-  render () {
+  render() {
     return (<div>
       <h1>Github Fetcher</h1>
       <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      <Search onSearch={this.search}/>
     </div>)
   }
 }
--- database/index.js
+++ database/index.js
@@ -1,16 +1,42 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Lawrence likes men')
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+const repoSchema = new mongoose.Schema({  //this is how you make the model
+  name: String,
+  repo: String,
+  fork: Number
+});
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+const Repo = mongoose.model('Repo', repoSchema); //this is how you make the collection
 
-module.exports.save = save;
+Repo.create({  //this is how you make the document
+  name: 'ryan',
+  repo: 'super-cool-repo',
+  fork: 1000000
+}, (err, result) => {
+  if (err) return console.log(err);
+  console.log(result);
+});
+
+// Repo.save( (err, Repo) => {
+//   if (err) return console.error(err);
+//     Repo()
+// })
+
+
+module.exports.Repo = Repo;
+
+
+//mongo has database, collection, document compared to mysql it is database, table, rows
+  //documnets is an object, it is just a mongo db thing
+  //columns will be like the keys - or properties
+  //it really is just a collection of objects
+  //we can use the model to make documnets of the model
+  //hey model, go make another model;
